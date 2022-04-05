a087d159b9db167be1da15b71b794ad18eb0e8eb
RaoulBock <anthonyraoulbock01@gmail.com>
2022-04-05 09:07:16 +0200

    front-end

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
-export default function App() {
+import AppProvider, { AppContext } from "./context/AppProvider";
+import { APP_PAGES } from "./context/settings";
+import { LoginView } from "./views/LoginView";
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppProvider>
+      <NavWrapper />
+    </AppProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const NavWrapper = () => {
+  const { navPage, setNavPage } = React.useContext(AppContext);
+
+  return (
+    <>
+      <View className="container">
+        <SafeAreaView>
+          <View className="CenterContent">
+            {navPage === APP_PAGES.INSTAGRAM.LOGIN && <LoginView />}
+          </View>
+        </SafeAreaView>
+      </View>
+    </>
+  );
+};
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center"
+//   }
+// });
+
+export default App;
