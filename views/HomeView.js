f4ffd6908d34517f8345c73aae9153462b8c611e
RaoulBock <anthonyraoulbock01@gmail.com>
2022-04-05 16:07:51 +0200

    front-end

--- views/HomeView.js
+++ views/HomeView.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 
 export const HomeView = () => {
+  const [isFollowing, setIsFollowing] = React.useState(false);
   return (
-    <View>
-      <Text>HomeView</Text>
-    </View>
+    <>
+      <SafeAreaView style={styles.topSection}>
+        {isFollowing ? (
+          <View>
+            <Text>Starting following your friends or your favroute artist</Text>
+          </View>
+        ) : (
+          <View>
+            <Text>HomeView</Text>
+          </View>
+        )}
+      </SafeAreaView>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  topSection: {
+    marginTop: "10%",
+    alignItems: "center"
+  }
+});
